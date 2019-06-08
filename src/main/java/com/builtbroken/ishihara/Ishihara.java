c5b1902ca2e73556207033cf78466a3e4993a731
Robert WC Seifert <rseifert.phone@gmail.com>
2019-06-08 18:03:10 -0400

    Added version injection

--- src/main/java/com/builtbroken/ishihara/Ishihara.java
+++ src/main/java/com/builtbroken/ishihara/Ishihara.java
@@ -29,7 +29,14 @@ import java.util.function.BiConsumer;
 public class Ishihara
 {
     public static final String MODID = "ishihara";
-    public static final String VERSION = "1.0";
+
+    public static final String MAJOR_VERSION = "@MAJOR@";
+    public static final String MINOR_VERSION = "@MINOR@";
+    public static final String REVISION_VERSION = "@REVIS@";
+    public static final String BUILD_VERSION = "@BUILD@";
+    public static final String MC_VERSION = "@MC@";
+    public static final String VERSION = MC_VERSION + "-" + MAJOR_VERSION + "." + MINOR_VERSION + "." + REVISION_VERSION + "." + BUILD_VERSION;
+
     public static Logger logger = LogManager.getLogger(MODID);
 
     private static final Minecraft mc = Minecraft.getMinecraft();
